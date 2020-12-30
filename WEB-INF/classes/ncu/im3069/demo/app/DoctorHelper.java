ee605b45d6a4ab15946acab321eeea66e3bcd28f
Franktsaiii <franktsai002@gmail.com>
2020-12-30 23:33:17 +0800

    add getByName()
    新增一個 物件getByName，用以透過醫師名字取得資料

--- WEB-INF/classes/ncu/im3069/demo/app/DoctorHelper.java
+++ WEB-INF/classes/ncu/im3069/demo/app/DoctorHelper.java
@@ -418,6 +418,92 @@ public class DoctorHelper {
 
         return response;
     }
+
+    /**
+     * 透過醫師名字（name）取得醫師資料
+     *
+     * @param name 醫師名字
+     * @return the JSON object 回傳SQL執行結果與該醫師編號之醫師資料
+     */
+    public JSONObject getByName(String name) {
+        /** 新建一個 Doctor 物件之 d 變數，用於紀錄每一位查詢回之醫師資料 */
+        Doctor d = null;
+        /** 用於儲存所有檢索回之醫師，以JSONArray方式儲存 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `sa_project`.`doctor` WHERE `name` = ? LIMIT 1";
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setString(4, name);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            /** 正確來說資料庫只會有一筆該醫師編號之資料，因此其實可以不用使用 while 迴圈 */
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+                
+                /** 將 ResultSet 之資料取出 */
+                int id = rs.getInt("id");
+                String doctor_name = rs.getString("name");
+                String account = rs.getString("account");
+                String password = rs.getString("password");
+                String dob = rs.getString("dob");
+                int phone = rs.getInt("phone");
+                String address = rs.getString("address");
+                /**Timestamp create_date = rs.getTimestamp("create_date");
+                *Timestamp modify_date = rs.getTimestamp("modify_date");
+                */
+
+                /** 將每一筆醫師資料產生一名新Member物件 */
+                d = new Doctor(id, doctor_name, account, password, dob, phone, address);
+                /** 取出該名醫師之資料並封裝至 JSONsonArray 內 */
+                jsa.put(d.getData());
+            }
+            
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間、影響行數與所有醫師資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
     
     /**
      * 檢查該名醫師之帳號是否重複註冊
