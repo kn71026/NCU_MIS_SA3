36f0d4bd851242506353693e1d3fc005c2001d87
chung-coder <peggy881203@gmail.com>
2021-01-03 00:14:50 +0800

    更新Appointment

--- WEB-INF/classes/ncu/im3069/demo/app/Appointment.java
+++ WEB-INF/classes/ncu/im3069/demo/app/Appointment.java
@@ -2,29 +2,31 @@ package ncu.im3069.demo.app;
 
 import org.json.*;
 import java.sql.*;
-import java.time.LocalDateTime;
 
 public class Appointment {
 
-    /** id，會員編號 */
+    /** id，病患編號 */
     private int id;
 
-    /** name，會員姓名 */
+    /** name，病患姓名 */
     private String name;
 
+    /** pid，病患身份證字號 */
     private String pid;
 
-    /** password，會員密碼 */
+    /** dob，病患生日 */
     private String dob;
 
-    /** login_times，更新時間的分鐘數 */
+    /** visited_date，病患看診日期 */
     private Timestamp visited_date;
 
-    /** status，會員之組別 */
+    /** appointment_number，病患看診序號 */
     private int appointment_number;
 
+    /** clinic_hours，病患看診時段 */
     private String clinic_hours;
 
+    /** done，病患看診完成紀錄 */
     private boolean done;
 
     /** mh，MemberHelper之物件與Member相關之資料庫方法（Sigleton） */
@@ -32,13 +34,7 @@ public class Appointment {
 
     /**
      * 實例化（Instantiates）一個新的（new）Order 物件<br>
-     * 採用多載（overload）方法進行，此建構子用於建立掛號資料時，產生一個新的掛號
-     *
-     * @param pid          病患身分證字號
-     * @param name         病患姓名
-     * @param dob          病患出生日期
-     * @param visited_date 病患看診日期
-     * @param clinic_hours 病患看診時段
+     * 採用多載（overload）方法進行，此建構子用於新增掛號
      */
 
     public Appointment(String pid, String name, String dob, Timestamp visited_date, String clinic_hours) {
@@ -51,12 +47,7 @@ public class Appointment {
 
     /**
      * 實例化（Instantiates）一個新的（new）Order 物件<br>
-     * 採用多載（overload）方法進行，此建構子用於刪除訂單資料時，刪除資料庫已存在的掛號
-     *
-     * @param name               會員名
-     * @param pid                會員姓
-     * @param clinic_hours       會員電子信箱
-     * @param appointment_number 會員地址
+     * 採用多載（overload）方法進行，此建構子用於刪除掛號、處理過號、補報到時
      */
     public Appointment(String pid, String name, String clinic_hours, int appointment_number, boolean done) {
         this.pid = pid;
@@ -67,106 +58,113 @@ public class Appointment {
     }
 
     /**
-     * 取得掛號之編號
+     * 實例化（Instantiates）一個新的（new）Medicine 物件<br>
+     * 採用多載（overload）方法進行，此建構子用於查詢掛號時
+     *
+     */
+    public Appointment(String pid, String name, boolean done) {
+        this.pid = pid;
+        this.name = name;
+        this.done = done;
+    }
+
+    /**
+     * 取得掛號編號
      *
      * @return the id 回傳掛號掛號編號
      */
-    public int getId() {
+    public int getID() {
         return this.id;
     }
 
     /**
-     * 取得會員之編號
+     * 取得病患姓名
      *
-     * @return the name 回傳會員編號
+     * @return the name 回傳病患姓名
      */
     public String getName() {
         return this.name;
     }
 
     /**
-     * 取得會員之編號
+     * 取得病患身份證字號
      *
-     * @return the id 回傳會員編號
+     * @return the pid 回傳病患身份證字號
      */
-    public String getPid() {
+    public String getPID() {
         return this.pid;
     }
 
     /**
-     * 取得會員之編號
+     * 取得病患看診日期
      *
-     * @return the id 回傳會員編號
+     * @return the visited_date 回傳病患看診日期
      */
     public Timestamp getVisitDate() {
         return this.visited_date;
     }
 
     /**
-     * 取得會員之編號
+     * 取得病患看診序號
      *
-     * @return the id 回傳會員編號
+     * @return the appointment_number 回傳病患看診序號
      */
     public int getAppointmentNumber() {
         return this.appointment_number;
     }
 
     /**
-     * 取得會員之編號
-     *
-     * @return the id 回傳會員編號
+     * 設定病患看診序號
      */
     public void setAppointmentNumber(int appointment_number) {
         this.appointment_number = appointment_number;
     }
 
     /**
-     * 取得會員之編號
+     * 取得病患看診時段
      *
-     * @return the id 回傳會員編號
+     * @return the clinic_hours 回傳病患看診時段
      */
     public String getClinicHours() {
         return this.clinic_hours;
     }
 
     /**
-     * 取得會員之編號
+     * 取得病患生日
      *
-     * @return the id 回傳會員編號
+     * @return the dob 回傳病患生日
      */
     public String getDob() {
         return this.dob;
     }
 
     /**
-     * 取得會員之編號
+     * 取得病患看診完成紀錄
      *
-     * @return the id 回傳會員編號
+     * @return the done 回傳病患看診完成紀錄
      */
     public boolean getDone() {
         return this.done;
     }
 
     /**
-     * 取得會員之編號
-     *
-     * @return the id 回傳會員編號
+     * 設定病患看診完成紀錄
      */
     public void setDone(boolean done) {
         this.done = done;
     }
 
     /**
-     * 取得該名會員所有資料
+     * 取得病患掛號資訊
      *
-     * @return the data 取得該名會員之所有資料並封裝於JSONObject物件內
+     * @return JSONObject 回傳掛號資訊
      */
     public JSONObject getData() {
         /** 透過JSONObject將該名會員所需之資料全部進行封裝 */
         JSONObject jso = new JSONObject();
-        jso.put("id", getId());
+        jso.put("id", getID());
         jso.put("name", getName());
-        jso.put("pid", getPid());
+        jso.put("pid", getPID());
         jso.put("visited_date", getVisitDate());
         jso.put("appointment_number", getAppointmentNumber());
         jso.put("clinic_hours", getClinicHours());
